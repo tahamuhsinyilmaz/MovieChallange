500efcbbbaacddb1ec0818263283e43df09aa380
Taha Muhsin YILMAZ <taha.yilmaz@onedio.com>
2020-09-23 21:16:30 +0300

    Initial Commit

---
+++ MovieChallange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieChallange
+//
+//  Created by onedio on 23.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
